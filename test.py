bd49ad11b6d4f6ee22ecb019d13e995e5467319a
Gulrukh07 <gulrukhkhalilovna@gmail.com>
2024-12-08 16:54:25 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Gulrukh07/Gulrukh.git
+git push -u origin main
+
+git config --global user.email "gulrukhkhalilovna@gmail.com"
+git config --global user.name "Gulrukh07"
+
+"""
